4cb99c9dff7b85b48c8e3208812a452cf2b91cdf
Hunter <1737087925@qq.com>
2019-03-05 10:22:34 +0800

    :bug: (template/bin/webpack.prod.conf.js) fix jade-url-replace-loader can't replace stylus
    fix jade-url-replace-loader can't repla

--- template/bin/webpack.prod.conf.js
+++ template/bin/webpack.prod.conf.js
@@ -5,7 +5,7 @@ const plugins = require('./plugins')
 const prodPlugins = plugins.getProdHelperPlugins()
 
 const jsManifest = plugins.getManifestPlugin(assetsRoot, publicPath, 'js')
-const cssManifest = plugins.getManifestPlugin(assetsRoot, publicPath, 'css')
+const cssManifest = plugins.getManifestPlugin(assetsRoot, publicPath, 'stylus')
 
 baseConfs[0].plugins = prodPlugins.concat(baseConfs[0].plugins).concat(jsManifest)
 baseConfs[1].plugins = prodPlugins.concat(baseConfs[1].plugins).concat(cssManifest)
